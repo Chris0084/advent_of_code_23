f1a1967103a3ae6df0d6285c92236f45c9162cc4
Ewart, Christopher (Tesco Bank) <>
2023-12-06 11:28:32 +0000

    set up

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+path = os.path.abspath("advent_of_code_files")
+sys.path.append(path)
+from AOC_Day1 import print_hi
+
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Chris')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
