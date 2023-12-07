9a3e66968b60eeb7a319aabfb93c8a7b3412c5bc
Ewart, Christopher (Tesco Bank) <>
2023-12-07 10:36:56 +0000

    day 1 part b

--- advent_of_code_files/AOC_Day1.py
+++ advent_of_code_files/AOC_Day1.py
@@ -4,7 +4,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 def is_number(digit):
-    if (digit == "1" or digit == "2" or digit == "3" or digit == "4" or digit == "5" or digit == "6" or digit == "7" or digit == "8" or digit == "9"):
+    if (digit == "0" or digit == "1" or digit == "2" or digit == "3" or digit == "4" or digit == "5" or digit == "6" or digit == "7" or digit == "8" or digit == "9"):
         return True
 
 def read_digit(digit):
@@ -20,7 +20,26 @@ def array_of_numbers(word):
 
     return num_array
 
-
+def convert_text_to_number(word):
+    new_word = word.replace("zerone", "01")
+    new_word = new_word.replace("oneight", "18")
+    new_word = new_word.replace("twone", "21")
+    new_word = new_word.replace("threeight", "38")
+    new_word = new_word.replace("fiveight", "58")
+    new_word = new_word.replace("eightwo", "82")
+    new_word = new_word.replace("eighthree", "83")
+    new_word = new_word.replace("nineight", "98")
+    new_word = new_word.replace("one", "1")
+    new_word = new_word.replace("two", "2")
+    new_word = new_word.replace("three", "3")
+    new_word = new_word.replace("four", "4")
+    new_word = new_word.replace("five", "5")
+    new_word = new_word.replace("six", "6")
+    new_word = new_word.replace("seven", "7")
+    new_word = new_word.replace("eight", "8")
+    new_word = new_word.replace("nine", "9")
+    new_word = new_word.replace("zero", "0")
+    return new_word
 
 
 
@@ -34,7 +53,21 @@ def day1():
         two_digit_value = int(str(first_digit) + str(second_digit))
         total = total + two_digit_value
 
-           
-
     file.close()  
     print(f'Total is, {total}')  
+
+
+def day1_part_b():
+    total = 0
+    file = open("advent_of_code_files/day1_input_file.txt", "r")
+    for word in file:
+        word = convert_text_to_number(word)
+        print(word)
+        numbers_array = array_of_numbers(word)
+        first_digit = numbers_array[0]
+        second_digit = numbers_array[-1]    
+        two_digit_value = int(str(first_digit) + str(second_digit))
+        total = total + two_digit_value
+
+    file.close()  
+    print(f'Total is, {total}')     
--- main.py
+++ main.py
@@ -2,19 +2,11 @@ import sys
 import os
 path = os.path.abspath("advent_of_code_files")
 sys.path.append(path)
-from AOC_Day1 import print_hi, day1
+from AOC_Day1 import print_hi, day1, day1_part_b
 
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-
-
-# Press the green button in the gutter to run the script.
+# uncomment the method you want to run below
 if __name__ == '__main__':
-    day1()
+    # day1()
+    day1_part_b()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
